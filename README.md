a61fa23bb9d2f506b79d0305ac01c46408a75fb8
Leesugyoung <tnrud4685@naver.com>
2024-10-14 22:50:57 +0900

    - settings

---
+++ README.md
@@ -0,0 +1,13 @@
+# TalkTee
+
+### Node.js -v
+
+```
+v20.9.0
+```
+
+### CSS
+
+```
+Tailwind
+```
---
+++ src/app/hooks/useAxios.tsx
@@ -0,0 +1,46 @@
+import { useEffect } from "react";
+import axios, {
+  AxiosResponse,
+  AxiosError,
+  InternalAxiosRequestConfig,
+} from "axios";
+
+export const BASE_URL = import.meta.env.VITE_IP;
+
+export const api = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+interface UseAxiosProps {
+  baseURL?: string;
+}
+
+export default function useAxios(_props: UseAxiosProps) {
+  const onRequest = async (config: InternalAxiosRequestConfig) => {
+    return config;
+  };
+
+  const onResponse = (response: AxiosResponse): AxiosResponse => {
+    return response;
+  };
+
+  const onError = async (error: AxiosError): Promise<never> => {
+    return Promise.reject(error);
+  };
+
+  const requestInterceptor = api.interceptors.request.use(onRequest);
+  const responseInterceptor = api.interceptors.response.use(
+    response => onResponse(response),
+    error => onError(error)
+  );
+
+  useEffect(() => {
+    return () => {
+      api.interceptors.request.eject(requestInterceptor);
+      api.interceptors.response.eject(responseInterceptor);
+    };
+  }, [responseInterceptor, requestInterceptor]);
+}
