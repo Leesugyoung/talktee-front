41d3edf3e87401b7f96a71b81c88b2d42e9c0896
Leesugyoung <tnrud4685@naver.com>
2024-10-27 23:51:06 +0900

    + header layout

--- src/component/layout/header/Header.tsx
+++ src/component/layout/header/Header.tsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+import titleImg from "../../../assets/image/타이틀이미지.png";
+
 const Header = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <header className="h-60 bg-gray-800 text-white flex items-center justify-center">
-      Header
+    <header className="h-60 bg-white text-white flex items-center justify-between px-4">
+      <img src={titleImg} width={"120px"} className="flex-shrink-0" />
+      <button>
+        <i
+          className="fa-solid fa-bars"
+          style={{ color: "#2b70af", fontSize: "1.5rem" }}
+        ></i>
+      </button>
     </header>
   );
 };
