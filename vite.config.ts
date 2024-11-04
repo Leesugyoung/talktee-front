5354e61ac3f0eb55881e8bcd1d138d4705cbb323
Leesugyoung <tnrud4685@naver.com>
2024-11-04 22:10:00 +0900

    + vite setting

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { resolve } from "path";
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    legacy({
+      targets: ["edge >= 93", "chrome >= 93"],
+      modernPolyfills: true,
+    }),
+  ],
   resolve: {
     alias: [
       { find: "@src", replacement: resolve(__dirname, "src") },
@@ -13,6 +22,21 @@ export default defineConfig({
       },
     ],
   },
-
-  plugins: [react(), tsconfigPaths()],
+  build: {
+    commonjsOptions: { transformMixedEsModules: true },
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
